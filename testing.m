f6fa77adb17ee1a537c60b3ea4aa1523e70b6314
adgoldha <146307216+adgoldha@users.noreply.github.com>
2024-03-14 22:58:53 -0700

    Final

--- testing.m
+++ testing.m
@@ -1,6 +1,6 @@
 clear all;
-n = 11;
-num_centroids = 8;
+n = 47;
+num_centroids = 40;
 
 signals = training(n, num_centroids);
 
@@ -9,14 +9,15 @@ signals = training(n, num_centroids);
 %     signals(i).plot_centroids(i)
 % end
 
-m = 8;
+m = 44;
+correct = 0;
 for i = 1:m
     % fprintf("Signal %d", i);
     file = sprintf("s%d.wav",i);
     current(i) = signal(file);
     current(i) = current(i).getMFCC();
 
-	min_dist = 10000;
+	min_dist = 100000;
 	match = 0;
 	for j = 1:n
 		dist = disteu(current(i).MFCC, (signals(j).clusters)');
@@ -32,4 +33,9 @@ for i = 1:m
 		% [rd1,cd1] = size(dist);
     end
     fprintf("Test Signal %d matches with Train Signal %d\n", i, match);
+    if i == match
+        correct = correct + 1;
+    end
 end
+accuracy = (correct/m)*100;
+fprintf("Accuracy of everything is %.3f%%\n", accuracy);
